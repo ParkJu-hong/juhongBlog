22ee24b484c3506cab5a20f095b3324110bf7079
bejeju <bejejupark@gmail.com>
2023-10-16 22:15:52 +0900

    user api update for using userService

--- src/main/java/juhongBlog/juhongBlog/controller/UserController.java
+++ src/main/java/juhongBlog/juhongBlog/controller/UserController.java
@@ -1,114 +1,48 @@
 package juhongBlog.juhongBlog.controller;
 
 import juhongBlog.juhongBlog.domain.User;
-import juhongBlog.juhongBlog.jwt.JwtTokenProvider;
-import juhongBlog.juhongBlog.repository.UserRepository;
+import juhongBlog.juhongBlog.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
+    private UserService userService;
 
     // 사용자 정보 조회
     @GetMapping("/findUser")
     @ResponseBody
     public User findUser(@RequestBody Map<String, String> user) {
-        if(user.get("email") != null) {
-            User userFined = userRepository.findByEmail(user.get("email"))
-                    .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
-            User result = User.builder()
-                    .email(userFined.getEmail())
-                    .password("")
-                    .id(userFined.getId())
-                    .roles(userFined.getRoles())
-                    .name(userFined.getName())
-                    .build();
-
-            return result;
-        }else if(user.get("name") != null) {
-            User userFined = userRepository.findByName(user.get("name"))
-                    .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
-            User result = User.builder()
-                    .email(userFined.getEmail())
-                    .password("")
-                    .id(userFined.getId())
-                    .roles(userFined.getRoles())
-                    .name(userFined.getName())
-                    .build();
-
-            return result;
-        }else {
-            return new User();
-        }
+        return userService.findUser(user.get("email"), user.get("name"));
     }
 
     // 회원가입
     @PostMapping("/join")
     @ResponseBody
     public Long join(@RequestBody Map<String, String> user) {
-        return userRepository.save(User.builder()
-                .email(user.get("email"))
-                .password(passwordEncoder.encode(user.get("password")))
-                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
-                .name(user.get("name"))
-                .build()).getId();
-    }
-
-    @GetMapping("/test")
-    @ResponseBody
-    public String testOne(@RequestHeader("X-AUTH-TOKEN") String token) {
-        if( jwtTokenProvider.validateToken(token)) {
-            return "토큰 유효함";
-        }else {
-            return "토큰 유효하지 않음";
-        }
+        return userService.join(user.get("email"), user.get("password"), user.get("name"));
     }
 
     // 로그인
     @PostMapping("/login")
     @ResponseBody
     public String login(@RequestBody Map<String, String> user) {
-        User member = userRepository.findByEmail(user.get("email"))
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+        return userService.login(user.get("email"), user.get("password"));
     }
 
     // 비밀번호 변경
-    @PostMapping("/findEmail")
+    @PostMapping("/chagepassword")
     @ResponseBody
-    public String findPassWord(@RequestBody Long userId, String beforePassword,String afterPassWord) {
-        User member = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
-        if (!passwordEncoder.matches(beforePassword, member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-
-        if(userId == member.getId()){
-            userRepository.save(User.builder()
-                    .email(member.getEmail())
-                    .name(member.getName())
-                    .id(member.getId())
-                    .roles(member.getRoles())
-                    .password(afterPassWord)
-                    .build()
-            );
-            return "비밀번호가 정상적으로 변경되었습니다.";
-        }else {
-            return "일치하는 userId가 없습니다.";
-        }
+    public String changePassWord(@RequestBody Map<String, String> user) {
+        return userService.changePassword(Long.parseLong(user.get("userId")), user.get("beforePassWord"), user.get("afterPassWord"));
     }
 
 }
--- src/main/java/juhongBlog/juhongBlog/service/UserService.java
+++ src/main/java/juhongBlog/juhongBlog/service/UserService.java
@@ -1,25 +1,107 @@
 package juhongBlog.juhongBlog.service;
 
+import juhongBlog.juhongBlog.domain.User;
+import juhongBlog.juhongBlog.jwt.JwtTokenProvider;
+import juhongBlog.juhongBlog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    // 회원가입
-    public String signIn() {
-        return "";
-    }
-
-    // 로그인
-
-    // 아이디 찾기
-
-    // 비밀번호 찾기
-
-    // 비밀번호 변경
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
     // 사용자 정보 조회
+    public User findUser(String email, String name) {
+        if(email != null) {
+            User userFined = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
+            User result = User.builder()
+                    .email(userFined.getEmail())
+                    .password("")
+                    .id(userFined.getId())
+                    .roles(userFined.getRoles())
+                    .name(userFined.getName())
+                    .build();
+
+            return result;
+        }else if(name != null) {
+            User userFined = userRepository.findByName(name)
+                    .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
+            User result = User.builder()
+                    .email(userFined.getEmail())
+                    .password("")
+                    .id(userFined.getId())
+                    .roles(userFined.getRoles())
+                    .name(userFined.getName())
+                    .build();
+
+            return result;
+        }else {
+            return new User();
+        }
+    }
+
+    // 회원가입
+    public Long join(String email, String password, String name){
+        return userRepository.save(User.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .name(name)
+                .build()).getId();
+    }
+
+    public String login(String email, String password){
+        User member = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
+
+    // 비밀번호 변경
+    public String changePassword(Long userId, String beforePassword,String afterPassWord){
+        User member = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 userId 입니다."));
+        if (!passwordEncoder.matches(beforePassword, member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        if(userId == member.getId()){
+            userRepository.save(User.builder()
+                    .email(member.getEmail())
+                    .name(member.getName())
+                    .id(member.getId())
+                    .roles(member.getRoles())
+                    .password(afterPassWord)
+                    .build()
+            );
+            return "비밀번호가 정상적으로 변경되었습니다.";
+        }else {
+            return "일치하는 userId가 없습니다.";
+        }
+    }
 
 }
+
+/*
+* 토큰 검증 하는 법
+* //
+//    @GetMapping("/test")
+//    @ResponseBody
+//    public String testOne(@RequestHeader("X-AUTH-TOKEN") String token) {
+//        if( jwtTokenProvider.validateToken(token)) {
+//            return "토큰 유효함";
+//        }else {
+//            return "토큰 유효하지 않음";
+//        }
+//    }
+* */
